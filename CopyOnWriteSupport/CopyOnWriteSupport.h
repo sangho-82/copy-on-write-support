7bd8b37a35542217b7c4f3f860805f2e631481a0
SangHo Cha <ipnetwork82@gmail.com>
2023-10-17 16:38:23 +0900

    Initial Commit

---
+++ CopyOnWriteSupport/CopyOnWriteSupport.h
@@ -0,0 +1,18 @@
+//
+//  CopyOnWriteSupport.h
+//  CopyOnWriteSupport
+//
+//  Created by SangHo Cha on 10/17/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CopyOnWriteSupport.
+FOUNDATION_EXPORT double CopyOnWriteSupportVersionNumber;
+
+//! Project version string for CopyOnWriteSupport.
+FOUNDATION_EXPORT const unsigned char CopyOnWriteSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CopyOnWriteSupport/PublicHeader.h>
+
+
